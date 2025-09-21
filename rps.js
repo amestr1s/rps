5e7634239abfa6d43cebf2097eae0fd00f9e19be
amestr1s <230774380+amestr1s@users.noreply.github.com>
2025-09-21 12:52:52 +0300

    Add functions for computer choice

--- rps.js
+++ rps.js
@@ -1,1 +1,18 @@
 console.log("Hello World")
+let max = 3;
+function getComputerChoice (max) {
+    return Math.floor(Math.random() * max);
+}
+let choice = getComputerChoice(max);
+console.log (choice);
+
+function getString (choice) {
+    if (choice === 0) {
+    choice = "rock";
+} else if (choice === 1) {
+    choice = "paper";
+} else {
+    choice = "scissors";
+} return choice;
+}
+console.log(getString(choice));
