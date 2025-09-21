406ac7b0bd57f9f40d329d9052d23513ed3054e4
amestr1s <230774380+amestr1s@users.noreply.github.com>
2025-09-21 13:34:51 +0300

    Add getHumanChoice function

--- rps.js
+++ rps.js
@@ -1,10 +1,11 @@
 console.log("Hello World")
 let max = 3;
+
 function getComputerChoice (max) {
     return Math.floor(Math.random() * max);
 }
 let choice = getComputerChoice(max);
-console.log (choice);
+console.log (choice); //checking correct string conversion
 
 function getString (choice) {
     if (choice === 0) {
@@ -16,3 +17,20 @@ function getString (choice) {
 } return choice;
 }
 console.log(getString(choice));
+
+let hchoice = prompt("Choose and enter a value between rock, paper, scissors");
+console.log(hchoice); //checking correct input transition through getHumanChoice
+
+function getHumanChoice (hchoice) {
+    if (hchoice === "rock") {
+    hchoice = hchoice;
+    } else if (hchoice === "paper") {
+    hchoice = hchoice;
+    } else if (hchoice === "scissors") {
+    hchoice = hchoice;
+    } else {
+    hchoice = "invalid reload the page";
+    } return hchoice;
+}
+
+console.log(getHumanChoice(hchoice));
