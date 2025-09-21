123513947011b01d8d324df748a5b5018fac3c9e
amestr1s <230774380+amestr1s@users.noreply.github.com>
2025-09-21 16:52:21 +0300

    Project done
    Completed project rock paper scissors from TOP.

--- rps.js
+++ rps.js
@@ -1,26 +1,38 @@
-console.log("Hello World")
+//console.log("Hello World")
 let max = 3;
 
 function getComputerChoice (max) {
     return Math.floor(Math.random() * max);
 }
 let choice = getComputerChoice(max);
-console.log (choice); //checking correct string conversion
+//console.log (choice); //checking correct string conversion
 
 function getString (choice) {
     if (choice === 0) {
     choice = "rock";
+//    console.log(choice);
 } else if (choice === 1) {
     choice = "paper";
+//    console.log(choice);
 } else {
     choice = "scissors";
-} return choice;
+//    console.log(choice);
+} 
+    console.log("PC rolls " + (choice));
+    return choice;
 }
-console.log(getString(choice));
 
-let hchoice = prompt("Choose and enter a value between rock, paper, scissors");
-console.log(hchoice); //checking correct input transition through getHumanChoice
-hchoice = hchoice.toLowerCase();
+
+let value = 0;
+
+function ask (value) {
+    value = prompt("Choose and enter a value between rock, paper, scissors");
+    value = value.toLowerCase();
+    return value;
+}
+
+let hchoice = ask (value);
+//console.log(hchoice); //checking correct input transition through getHumanChoice
 
 function getHumanChoice (hchoice) {
     if (hchoice === "rock") {
@@ -30,15 +42,81 @@ function getHumanChoice (hchoice) {
     } else if (hchoice === "scissors") {
     hchoice = hchoice;
     } else {
-    hchoice = "invalid reload the page";
-    } return hchoice;
+    hchoice = "invalid value reload the page to start again";
+    } 
+    console.log("User chose " + (hchoice));
+    return hchoice;
 }
 
-console.log(getHumanChoice(hchoice));
+//console.log("User chose " + (hchoice));
 
 let humanScore = 0;
 let computerScore = 0;
 
 function playRound (humanChoice, computerChoice) {
-    
+    if (humanChoice === computerChoice) {
+    console.log("It's a tie!");
+    alert("It's a tie");
+    } else if ((humanChoice === "rock") && (computerChoice === "paper")) {
+    console.log("PC wins, paper beats rock");
+    computerScore = ++computerScore;
+    alert("You lose\nPaper beats rock");
+    } else if ((humanChoice === "rock") && (computerChoice === "scissors")) {
+    console.log("You win, rock beats scissors");
+    humanScore = ++humanScore;
+    alert("You win\nRock beats scissors");
+    } else if ((humanChoice === "paper") && (computerChoice === "rock")) {
+    console.log("You win, paper beats rock");
+    humanScore = ++humanScore;
+    alert("You win\nPaper beats rock");
+    } else if ((humanChoice === "paper") && (computerChoice === "scissors")) {
+    console.log("PC wins, scissors beat paper");
+    computerScore = ++computerScore;
+    alert("You lose\nScissors beat paper");
+    } else if ((humanChoice === "scissors") && (computerChoice === "paper")) {
+    console.log("You win, scissors beat paper");
+    humanScore = ++humanScore;
+    alert("You win\nScissors beat paper");
+    } else if ((humanChoice === "scissors") && (computerChoice === "rock")) {
+    console.log("PC wins, rock beats scissors");
+    computerScore = ++computerScore;
+    alert("You lose\nRock beats scissors");
+    } 
+    console.log(humanScore); //checking human score
+    console.log(computerScore); //checking computer score
+    console.log("Your points are " + (humanScore) + " and the PC points are " + (computerScore));
+    alert("Score now is user: " + (humanScore) + " PC: " + (computerScore));
+    return;
 }
+
+function playGame() {
+let computerChoice = getString(choice);
+let humanChoice = getHumanChoice(hchoice);
+playRound(humanChoice, computerChoice);
+choice = getComputerChoice(max);
+hchoice = ask(value);
+computerChoice = getString(choice);
+humanChoice = getHumanChoice(hchoice);
+playRound(humanChoice, computerChoice);
+choice = getComputerChoice(max);
+hchoice = ask(value);
+computerChoice = getString(choice);
+humanChoice = getHumanChoice(hchoice);
+playRound(humanChoice, computerChoice);
+choice = getComputerChoice(max);
+hchoice = ask(value);
+computerChoice = getString(choice);
+humanChoice = getHumanChoice(hchoice);
+playRound(humanChoice, computerChoice);
+choice = getComputerChoice(max);
+hchoice = ask(value);
+computerChoice = getString(choice);
+humanChoice = getHumanChoice(hchoice);
+playRound(humanChoice, computerChoice);
+}
+
+playGame();
+
+
+
+console.log("Your points are " + (humanScore) + " and the PC points are " + (computerScore));
